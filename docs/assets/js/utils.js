a24b5cb8df40fb6f57ea105f2152ec05d975dd04
Mara <mara-li@outlook.fr>
2022-01-26 18:36:25 +0100

    **Updated** :  Added : Illustration (Hayleen) in [Hayleen May]

--- docs/assets/js/utils.js
+++ docs/assets/js/utils.js
@@ -16,12 +16,27 @@ var p_search = /!?\[{2}(.*)\]{2}/gi
 var ht = document.querySelectorAll('article.md-content__inner.md-typeset > *:not(.highlight)');
 for (var i = 0; i < ht.length; i++) {
     const found_p = ht[i].innerHTML.match(p_search);
+    console.log(found_p)
     if (found_p) {
-        founded = heading(found_p[0])
+        founded = heading(found_p.join())
         var not_found = founded.replace(/]]/gi, '</div>');
         not_found = not_found.replace(/\[\[/gi, '<div class="not_found">')
         not_found = not_found.replace('!', '')
         ht[i].innerHTML = ht[i].innerHTML.replace(found_p, not_found);
+    }
+}
+
+// same for list
+var ht_list = document.querySelectorAll('li');
+for (var i = 0; i < ht_list.length; i++) {
+    const found_p = ht_list[i].innerHTML.match(p_search);
+    console.log(found_p)
+    if (found_p) {
+        founded = heading(found_p.join())
+        var not_found = founded.replace(/]]/gi, '</div>');
+        not_found = not_found.replace(/\[\[/gi, '<div class="not_found">')
+        not_found = not_found.replace('!', '')
+        ht_list[i].innerHTML = ht_list[i].innerHTML.replace(found_p, not_found);
     }
 }
 
