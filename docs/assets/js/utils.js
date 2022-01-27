7fd26a122557df7b372b94480e8850ce15e5eafa
Mara <mara-li@outlook.fr>
2022-01-27 23:21:07 +0100

    finally fix 404 display

--- docs/assets/js/utils.js
+++ docs/assets/js/utils.js
@@ -1,31 +1,32 @@
+function UrlExists(url) {
+    ref = url.href
+    var http = new XMLHttpRequest();
+    http.open('GET', ref, true);
+    http.onload=function(e) {
+        if (http.status == '404') {
+            const newItem = document.createElement('div');
+            newItem.innerHTML = url.innerHTML;
+            newItem.classList.add('not_found');
+            url.parentNode.replaceChild(newItem, url);
+        }
+        else {
+            return true;
+        }
+    }
+    http.send()
+}
 
 
-
-var p_search = /\.\.%5C/gi
+var p_search = /\.{2}\//gi
+not_found = []
 var ht = document.querySelectorAll('a');
 for (var i = 0; i < ht.length; i++) {
-    var link = ht[i].href.match(p_search);
-    if (link) {
-        const newItem = document.createElement('div');
-        newItem.innerHTML = ht[i].innerHTML;
-        newItem.classList.add('not_found');
-        ht[i].parentNode.replaceChild(newItem, ht[i]);
-    }
+    var link = UrlExists(ht[i]);
+
 }
 
 
-var p_img = /\.+\\/gi
-var img = document.querySelectorAll('img');
-var links = document.querySelector("link[rel='icon']").href.replace('assets/logo/favicons.png', '');
-for (var i = 0; i < img.length; i++) {
-    (img[i].attributes.src.nodeValue)
-    img[i].attributes.src.nodeValue = img[i].attributes.src.nodeValue.replace(/\.+\\/, links)
-    if (img[i].alt.match(/\|?\d+$/)) {
-        img[i].width = img[i].alt.match(/\|?\d+$/)[0].replace('|', '')
-    }
-}
-
-
+var ht = document.querySelectorAll('article.md-content__inner.md-typeset > *:not(.highlight)');
 var scr=/\^(.*)/gi;
 for (var i = 0; i <ht.length;i++){
     const fp=ht[i].innerHTML.match(scr)
