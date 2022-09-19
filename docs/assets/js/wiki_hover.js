141b4c541762c5de453274b733908440d1c7b6be
Mara <mara-li@outlook.fr>
2022-09-20 00:58:36 +0200

    tooltip success !!!!

--- docs/assets/js/wiki_hover.js
+++ docs/assets/js/wiki_hover.js
@@ -1,128 +1,6 @@
 const blogURL = location.origin = location.protocol + '//' + location.host;
 const template = document.querySelector(`.md-content a[href^="${blogURL}]"`)
-console.log(template)
-
-/**
- * [trimHtml description]
- * @param  {String} html
- * @param  {Object} options
- * @return {Object}
- */
-function trimHtml(html, options) {
-
-    options = options || {};
-
-    var limit = options.limit || 100,
-        preserveTags = (typeof options.preserveTags !== 'undefined') ? options.preserveTags : true,
-        wordBreak = (typeof options.wordBreak !== 'undefined') ? options.wordBreak : false,
-        suffix = options.suffix || '...',
-        moreLink = options.moreLink || '';
-
-    var arr = html.replace(/</g, "\n<")
-        .replace(/>/g, ">\n")
-        .replace(/\n\n/g, "\n")
-        .replace(/^\n/g, "")
-        .replace(/\n$/g, "")
-        .split("\n");
-
-    var sum = 0,
-        row, cut, add,
-        tagMatch,
-        tagName,
-        tagStack = [],
-        more = false;
-
-    for (var i = 0; i < arr.length; i++) {
-
-        row = arr[i];
-        // count multiple spaces as one character
-        rowCut = row.replace(/[ ]+/g, ' ');
-
-        if (!row.length) {
-            continue;
-        }
-
-        if (row[0] !== "<") {
-
-            if (sum >= limit) {
-                row = "";
-            } else if ((sum + rowCut.length) >= limit) {
-
-                cut = limit - sum;
-
-                if (row[cut - 1] === ' ') {
-                    while (cut) {
-                        cut -= 1;
-                        if (row[cut - 1] !== ' ') {
-                            break;
-                        }
-                    }
-                } else {
-
-                    add = row.substring(cut).split('').indexOf(' ');
-
-                    // break on halh of word
-                    if (!wordBreak) {
-                        if (add !== -1) {
-                            cut += add;
-                        } else {
-                            cut = row.length;
-                        }
-                    }
-                }
-
-                row = row.substring(0, cut) + suffix;
-
-                if (moreLink) {
-                    row += '<a href="' + moreLink + '" style="display:inline">»</a>';
-                }
-
-                sum = limit;
-                more = true;
-            } else {
-                sum += rowCut.length;
-            }
-        } else if (!preserveTags) {
-            row = '';
-        } else if (sum >= limit) {
-
-            tagMatch = row.match(/[a-zA-Z]+/);
-            tagName = tagMatch ? tagMatch[0] : '';
-
-            if (tagName) {
-                if (row.substring(0, 2) !== '</') {
-
-                    tagStack.push(tagName);
-                    row = '';
-                } else {
-
-                    while (tagStack[tagStack.length - 1] !== tagName && tagStack.length) {
-                        tagStack.pop();
-                    }
-
-                    if (tagStack.length) {
-                        row = '';
-                    }
-
-                    tagStack.pop();
-                }
-            } else {
-                row = '';
-            }
-        }
-
-        arr[i] = row;
-    }
-
-    return {
-        html: arr.join("\n").replace(/\n/g, ""),
-        more: more
-    };
-}
-
-if (typeof module !== 'undefined' && module.exports) {
-    module.exports = trimHtml;
-}
+let position = ['top', 'right', 'bottom', 'left'];
 
 const tip = tippy(`.md-content a[href^="${blogURL}"]`, {
     content:'',
@@ -131,7 +9,7 @@ const tip = tippy(`.md-content a[href^="${blogURL}"]`, {
     theme: 'translucent',
     followCursor: true,
     arrow: false,
-    placement: 'right',
+    placement: position[Math.floor(Math.random() * position.length)],
     onShow(instance) {
         fetch(instance.reference.href)
             .then(response => response.text())
@@ -139,8 +17,9 @@ const tip = tippy(`.md-content a[href^="${blogURL}"]`, {
                 const parser = new DOMParser();
                 const doc = parser.parseFromString(html, 'text/html');
                 const firstPara = doc.querySelector('article');
+                const element1 = document.querySelector(`[id^="tippy"]`);
+                element1.classList.add('tippy')
                 instance.setContent(firstPara);
-                
             })
             .catch(error => {
                 console.log(error);
@@ -148,3 +27,4 @@ const tip = tippy(`.md-content a[href^="${blogURL}"]`, {
             });
     }
 });
+
