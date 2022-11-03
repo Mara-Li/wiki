a18345ed32539dfa53e8c3307f7af05f3650f5b3
Mara <mara-li@outlook.fr>
2022-11-03 19:23:00 +0100

    fix hover wiki

--- docs/assets/js/wiki_hover.js
+++ docs/assets/js/wiki_hover.js
@@ -16,6 +16,15 @@ const tip = tippy(`.md-content a[href^="${blogURL}"]`, {
                 const parser = new DOMParser();
                 const doc = parser.parseFromString(html, 'text/html');
                 let firstPara = doc.querySelector('article');
+                //broken link in first para
+                const brokenImage = firstPara.querySelectorAll('img');
+                if (brokenImage) {
+                    for (let i = 0; i < brokenImage.length; i++) {
+                        const encodedImage = brokenImage[i];
+                        encodedImage.src = decodeURI(decodeURI(encodedImage.src));
+                        //replace broken image with encoded image in first para
+                    }
+                }
                 const element1 = document.querySelector(`[id^="tippy"]`);
                 if (element1) {
                     element1.classList.add('tippy')
@@ -33,7 +42,14 @@ const tip = tippy(`.md-content a[href^="${blogURL}"]`, {
                     instance.popper.style.height = 'auto';
                 }
                 else {
-                    instance.popper.style.height = `${Math.floor(firstPara.innerText.split(' ').length / 100) - 5}%`;
+                    const height = Math.floor(firstPara.innerText.split(' ').length / 100)
+                    if (height <5 && height > 3) {
+                        instance.popper.style.height = `50%`;
+                    } if (height < 3) {
+                        instance.popper.style.height = `auto`;
+                    } else if (height > 5) {
+                        instance.popper.style.height = `${height - 5}%`;
+                    }
                 }
                 instance.popper.placement = position[Math.floor(Math.random() * position.length)];
                 instance.setContent(firstPara);
