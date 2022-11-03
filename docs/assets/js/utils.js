9e9ef394593a322a2e37d9bedb5fd10dbe4a3b17
Mara <mara-li@outlook.fr>
2022-11-03 19:27:41 +0100

    trying

--- docs/assets/js/utils.js
+++ docs/assets/js/utils.js
@@ -88,3 +88,29 @@ if (cite) {
         }
     }
 }
+
+const mkDocsChirpyTranslator = {
+    default: 'light',
+    slate: 'dark',
+}
+
+const mkDocs = document.querySelector('[data-md-color-scheme]')
+const chirpy = document.querySelector('[data-chirpy-theme]')
+
+// create mutation observer to change chirpyTheme when mkDocs theme changes
+const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+        if (mutation.type === 'attributes') {
+            chirpy.setAttribute('data-chirpy-theme', mkDocsChirpyTranslator[mkDocs.dataset.mdColorScheme])
+        }
+    })
+})
+
+// observe mkDocs theme change
+observer.observe(mkDocs, {
+    attributes: true,
+    attributeFilter: ['data-md-color-scheme'],
+})
+
+//Add background color css for chirpy theme
+
