52a2bb41a443265defb9672c7a2a7e502ef712bd
Mara <mara-li@outlook.fr>
2023-01-06 23:58:10 +0100

    script to remove unused images

---
+++ find_unused_media.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+import os 
+
+docs_dir = Path(Path.cwd(), 'docs')
+
+#list images of assets
+assets_dir = Path(docs_dir, 'assets', 'img')
+images = [file for file in assets_dir.rglob('*') if file.is_file() and file.suffix in ['.png', '.jpg', '.jpeg', '.gif', '.svg']]
+md_files = [file for file in docs_dir.rglob('*.md') if file.is_file()]
+
+
+# Search for images in markdown files
+
+used_images = []
+
+for md_file in md_files:
+    for image in images:
+        with open(md_file, 'r', encoding='utf-8') as f:
+            if image.name in f.read():
+                used_images.append(image)
+
+# compare the two lists
+
+unused_images = [image for image in images if image not in used_images]
+
+# delete unused images
+
+print('Unused images:')
+for image in unused_images:
+    print(image)
+    os.remove(image)
